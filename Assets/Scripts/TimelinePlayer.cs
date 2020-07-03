dd559a79c0aae6c00466fd24a56347343aabff97
Jing <jtan1993@gmail.com>
2020-07-03 23:14:38 +0100

    added scripted timeline example

---
+++ Assets/Scripts/TimelinePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelinePlayer : MonoBehaviour
+{
+    private PlayableDirector director;
+    public GameObject controlPanel;
+
+
+    void Awake()
+    {
+        //set playable director reference, register the callback functions to be invoked when director start/stop
+        director = GetComponent<PlayableDirector>();
+        director.played += DirectorPlayed;
+        director.stopped += DirectorStopped;
+    }
+
+    private void DirectorStopped(PlayableDirector playableDirector)
+    {
+        controlPanel.SetActive(true); //show the ui panel after the timeline has stopped
+    }
+
+    private void DirectorPlayed(PlayableDirector playableDirector)
+    {
+        controlPanel.SetActive(false); //hide the ui used to start the timeline once timeline has started playing
+    }
+
+    public void StartTimeline()
+    {
+        director.Play();
+    }
+
+}
